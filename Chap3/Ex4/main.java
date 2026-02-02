e8f898f49536f45abad26611e90179ce9a9ec184
Gia Bao <tagiabao376@gmail.com>
2026-02-02 07:43:39 +0700

    Implement asynchronous order processing with CompletableFuture

--- Chap3/Ex4/main.java
+++ Chap3/Ex4/main.java
@@ -1,1 +1,39 @@
+import java.util.concurrent.CompletableFuture;
+
+public class main {
+    public static void main(String[] args) {
+
+        CompletableFuture<Void> checkProduct = CompletableFuture.runAsync(() -> {
+            System.out.println("Dang kiem tra san pham...");
+            sleep(2000);
+            System.out.println("Kiem tra san pham xong!");
+        });
+
+        CompletableFuture<Void> payment = CompletableFuture.runAsync(() -> {
+            System.out.println("Dang thanh toan...");
+            sleep(3000);
+            System.out.println("Thanh toan xong!");
+        });
+
+        CompletableFuture<Void> shipping = CompletableFuture.runAsync(() -> {
+            System.out.println("Dang van chuyen...");
+            sleep(4000);
+            System.out.println("Van chuyen xong!");
+        });
+
+        CompletableFuture.allOf(checkProduct, payment, shipping)
+                .thenRun(() -> {
+                    System.out.println("Don hang da duoc xu ly hoan tat!");
+                })
+                .join();
+    }
+
+    public static void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
