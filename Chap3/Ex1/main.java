c924d57a00ee5a431477339086dc89fa2e1d502d
Gia Bao <tagiabao376@gmail.com>
2026-02-02 07:42:00 +0700

    Add threading examples with WorkerThread and WorkerRunnable

--- Chap3/Ex1/main.java
+++ Chap3/Ex1/main.java
@@ -1,1 +1,25 @@
+class WorkerThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Thread dang chay...");
+    }
+}
+
+class WorkerRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Runnable dang chay...");
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+
+        WorkerThread t1 = new WorkerThread();
+        t1.start();
+
+        Thread t2 = new Thread(new WorkerRunnable());
+        t2.start();
+    }
+}
 
