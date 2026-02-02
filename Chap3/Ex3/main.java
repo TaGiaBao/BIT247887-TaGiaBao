0d3c12781a512da8a7c64a4b34f8c9b4d754ed68
Gia Bao <tagiabao376@gmail.com>
2026-02-02 07:43:12 +0700

    Update main.java

--- Chap3/Ex3/main.java
+++ Chap3/Ex3/main.java
@@ -1,1 +1,38 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+public class main {
+    public static void main(String[] args) {
+
+        CompletableFuture
+                .supplyAsync(() -> {
+
+                    int[] arr = {1, 2, 5, 3, 100};
+                    return arr;
+                })
+
+                .thenApply(arr -> {
+
+                    List<Integer> oddNumbers = Arrays.stream(arr)
+                            .filter(x -> x % 2 != 0)
+                            .sorted()
+                            .boxed()
+                            .collect(Collectors.toList());
+
+                    return oddNumbers;
+                })
+
+                .thenApply(result -> {
+
+                    return "Ket qua la: " + result;
+                })
+
+                .thenAccept(finalString -> {
+
+                    System.out.println(finalString);
+                });
+    }
+}
 
